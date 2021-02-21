36972779fdbdf1c7e236bc0581d424deccc297d6
Ilysa Sanchez-Perez <ims5280@psu.edu>
2021-02-20 21:24:54 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Ilysa Sanchez-Perez on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
